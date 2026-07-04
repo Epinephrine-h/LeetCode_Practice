4918fc1b858e5d5bcf232927d0294993a757e6c7
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-04 20:06:06 +0700

    Time: 3 ms (39.49%), Space: 12.5 MB (46.74%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,8 +5,6 @@ class Solution(object):
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
-                j += 1
-            else:
-                j += 1
+            j += 1
         return i == len(s)
         
