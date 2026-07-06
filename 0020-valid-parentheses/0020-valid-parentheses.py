4b7490a088c5cb69dcdb089c4e3d5772c37f8a6b
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-06 10:32:46 +0700

    Time: 1 ms (86.41%), Space: 12.5 MB (69.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,13 @@
 class Solution(object):
     def isValid(self, s):
         stack = []
-        openBrackets = {'[', '{', '('}
-        closeBrackets = {']', '}', ')'}
+        mapping = {']':'[', '}' : '{', ')' : '('}
         for i in s:
-            if i in openBrackets:
-                stack.append(i)
-            elif i in closeBrackets and len(stack) != 0:
-                if i == ']' and stack[-1] == '[':
-                    stack.pop()
-                elif i == ')' and stack[-1] == '(':
-                    stack.pop()
-                elif i == '}' and stack[-1] == '{':
-                    stack.pop()
-                else:
+            if i in mapping:
+                top_stack = stack.pop() if stack else '#'
+                if top_stack != mapping[i]:
                     return False
             else:
-                return False
-        if len(stack) != 0:
-            return False
-        return True
+                stack.append(i)
+        return not stack
         
