76e5187a399d046a3748891e2c5e6b4220ec495d
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-06 15:10:15 +0700

    Time: 0 ms (100%), Space: 12.8 MB (94.46%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,7 +3,7 @@ class Solution(object):
         left = 0
         right = len(nums) - 1
         while left <= right:
-            mid = (int)(left + right)/2
+            mid = int((left + right)/2)
             if (nums[mid] == target):
                 return mid
             elif nums[mid] > target:
