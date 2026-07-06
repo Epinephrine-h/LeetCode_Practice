1047b8986d4da13e39f87b7b1f3a33dcc4f6806c
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-06 22:31:50 +0700

    Time: 4 ms (79.68%), Space: 13.6 MB (73.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,10 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums.sort()
-        return nums[len(nums)/2]
+        candidate = None
+        cnt = 0
+        for i in nums:
+            if cnt == 0:
+                candidate = i
+            cnt += 1 if candidate == i else -1
+        return candidate
         
