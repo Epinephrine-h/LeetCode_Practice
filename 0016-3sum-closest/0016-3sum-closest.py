a09cf84a436de0789fbbc2daeed9f734d6e28c14
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-06 18:25:47 +0700

    Time: 500 ms (11.32%), Space: 12.4 MB (77.56%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -5,8 +5,7 @@ class Solution(object):
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left = i + 1
-            right = len(nums) - 1
+            left, right = i + 1, len(nums) - 1
             while left < right:
                 sum = nums[i] + nums[left] + nums[right]
                 if abs(sum - target) < abs(m - target):
