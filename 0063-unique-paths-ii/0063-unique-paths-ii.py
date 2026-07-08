0413accae089fa22707b7c9301dfc38d47361576
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-08 20:09:58 +0700

    Time: 0 ms (100%), Space: 19.4 MB (32.11%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,11 +1,18 @@
-from functools import lru_cache
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        @lru_cache
-        def recursion(a, b):
-            if a < 0 or b < 0 or obstacleGrid[a][b] == 1:
-                return 0
-            if a == 0 and b == 0:
-                return 1
-            return recursion(a-1, b) + recursion(a, b-1)
-        return recursion(len(obstacleGrid) - 1,len(obstacleGrid[0]) - 1)
+        if obstacleGrid[0][0] == 1:
+            return 0
+        dp = [[0] * len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
+        dp[0][0] = 1
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[i])):
+                if (i == 0 and j == 0)  or obstacleGrid[i][j] == 1:
+                    continue
+                elif i == 0:
+                    dp[i][j] += dp[i][j-1]
+                elif j == 0:
+                    dp[i][j] += dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+                
