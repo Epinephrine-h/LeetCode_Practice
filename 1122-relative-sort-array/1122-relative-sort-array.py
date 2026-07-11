c1f3fa0f231b1fe301870c2d19234bb7862e171b
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-11 19:41:36 +0700

    Time: 0 ms (100%), Space: 19.2 MB (84.34%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,12 +1,13 @@
+from collections import Counter
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        p2 = 0
-        p1 = 0
-        while p2 < len(arr2):
-            for i in range(p1, len(arr1)):
-                if arr1[i] == arr2[p2]:
-                    arr1[p1], arr1[i] = arr1[i], arr1[p1]
-                    p1 += 1
-            p2 += 1
-        arr1[p1:] = sorted(arr1[p1:])
-        return arr1
+        count = Counter(arr1)
+        res = []
+        for num in arr2:
+            res.extend([num]*count[num])
+            del count[num]
+        remaining = sorted(count.keys())
+        for num in remaining:
+            res.extend([num] * count[num])
+            del count[num]
+        return res
