8f98cbf1bb790f1bf8255af334242dba7df36560
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-12 18:37:03 +0700

    Time: 0 ms (100%), Space: 19.2 MB (61.86%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        x, y = sorted(list(s)), sorted(list(t))
-        for i in range(len(x)):
-            if x[i] != y[i]:
-                return y[i]
-        return y[-1]
+        res = 0
+        for ch in s + t:
+            res ^= ord(ch)
+        return chr(res)
