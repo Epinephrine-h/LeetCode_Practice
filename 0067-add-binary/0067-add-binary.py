71a8bb3433b237a49df23ca9f5b3bece20730c8e
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-12 09:05:17 +0700

    Time: 3 ms (41.37%), Space: 19.5 MB (16.65%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -3,8 +3,8 @@ class Solution:
         res = []
         pa, pb, carry = len(a) - 1, len(b) - 1, 0
         while pa >= 0 or pb >= 0 or carry !=0:
-            na = 0 if pa < 0 or a[pa] == '0' else 1
-            nb = 0 if pb < 0 or b[pb] == '0' else 1
+            na = int(a[pa]) if pa >= 0 else 0
+            nb = int(b[pb]) if pb >= 0 else 0
             sum = na + nb + carry
             carry = sum//2
             res.append(str(sum%2))
