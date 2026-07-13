8580e5e5b15ce39aa4107112e4c71ff5f4ec92e9
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-13 23:05:49 +0700

    Time: 0 ms (100%), Space: 19.3 MB (61.5%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,16 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        #crazyRabbit
-        def get_next(number):
-            total_sum = 0
-            while number:
-                digit = number % 10
-                total_sum += digit * digit
-                number = number // 10
-            return total_sum
-        turtle = n
-        rabbit = get_next(n)
-        while rabbit != 1 and rabbit != turtle:
-            turtle = get_next(turtle)
-            rabbit = get_next(get_next(rabbit))
-        return rabbit == 1
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            tmp = 0
+            while n:
+                digit = n % 10
+                tmp += digit * digit
+                n = n // 10
+            n = tmp
+        return n == 1
