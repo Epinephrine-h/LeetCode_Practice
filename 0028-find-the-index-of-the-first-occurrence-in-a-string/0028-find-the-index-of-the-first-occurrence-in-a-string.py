110cae67c8357564537375965032d7d9ef961b68
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-14 13:09:42 +0700

    Time: 0 ms (100%), Space: 19.2 MB (60.18%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,5 +3,5 @@ class Solution:
         if needle not in haystack:
             return -1
         for i in range(len(haystack) - len(needle) + 1):
-            if needle in haystack[i:i + len(needle)]:
+            if needle == haystack[i:i + len(needle)]:
                 return i
