1cd64564e61ea1d96c64ecb6fb4c6cf0e214d141
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-18 00:54:52 +0700

    Time: 50 ms (22.52%), Space: 19.3 MB (48.63%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,5 +1,5 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         texts = sorted(s.split(), key = lambda x : x[-1])
-        word = [t.replace(t[-1], "") for t in texts]
+        word = [t[:-1] for t in texts]
         return " ".join(word)
