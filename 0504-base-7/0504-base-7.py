c5400552894c70fdbbd42cd2179a71cafccd9f96
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-19 20:43:31 +0700

    Time: 0 ms (100%), Space: 19.4 MB (10.03%) - LeetHub

--- 0504-base-7/0504-base-7.py
+++ 0504-base-7/0504-base-7.py
@@ -1,13 +1,5 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num == 0:
-            return "0"
-        n = abs(num)
-        mod = []
-        while n:
-            x = n % 7
-            n //= 7
-            mod.append(str(x))
-        if num < 0:
-            mod.append('-')
-        return "".join(mod[::-1])
+        if num < 0:     return '-' + self.convertToBase7(-num)
+        if num < 7:     return str(num)
+        return self.convertToBase7(num//7) + str(num % 7)
