0991ff43bfbab53ffc7f8edf8423c5e484b3feb5
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-20 18:42:50 +0700

    Time: 11 ms (99.02%), Space: 58.8 MB (98.62%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,14 +1,4 @@
+from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        tmp = [i for i in range(1, k + 1)]
-        res = []
-        while True:
-            res.append(list(tmp))
-            i = k - 1
-            while tmp[i] == n - k + i + 1:
-                i -= 1
-            if i < 0:
-                return res
-            else:
-                tmp[i] += 1
-                tmp[i+1:] = [x for x in range(tmp[i] + 1, tmp[i] + k - i)]
+        return list(combinations(range(1, n+1), k))
