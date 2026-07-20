fe61f8157ec95033463a08ef0f8b8e288d034611
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-20 10:45:27 +0700

    Time: 53 ms (15.33%), Space: 19.1 MB (69.41%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,6 +4,6 @@ class Solution:
         while n:
             r = r * 10 + n % 10
             n //= 10
-        if -(1 << 31) > r or r > (1 << 31) - 1:
-            return 0
+            if r > (1 << 31) - 1:
+                return 0
         return -r if x < 0 else r
