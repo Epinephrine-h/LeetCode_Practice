3ce253132e5fcb1a7b1722d8144b4b92f255c8e9
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-25 00:42:56 +0700

    Time: 6 ms (87.59%), Space: 19.3 MB (46.6%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,14 +1,12 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:    return s
-        ans = []
-        for i in range(numRows):
-            floor = i
-            down = False if i != numRows - 1 else   True
-            for j in range(i,len(s)):
-                if floor == i:  ans.append(s[j])
-                if down:  floor -= 1
-                else:   floor += 1
-                if floor == 0:  down = False
-                if floor == numRows - 1:    down = True
-        return "".join(ans)
+        if numRows == 1 or numRows > len(s):     return s
+        cur_row = 0
+        direction = 0
+        row = [""]*numRows
+        for ch in s:
+            row[cur_row] += ch
+            if cur_row == 0:    direction = 1
+            if cur_row == numRows - 1:   direction = -1
+            cur_row += direction
+        return "".join(row)
