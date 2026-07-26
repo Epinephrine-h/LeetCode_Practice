8584f43aa5e298e7b078c642009e6d07288f7041
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-26 15:26:24 +0700

    Time: 0 ms (100%), Space: 19.2 MB (68.64%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:  return nums[-1]
-        def rob1(start, end):
+        def rob1(house):
             prev2 = prev1 = 0
-            for money in nums[start:end]:
+            for money in house:
                 curr = max(prev2 + money, prev1)
                 prev2, prev1 = prev1, curr
             return prev1
-        return max(rob1(1,len(nums)), rob1(0, len(nums) - 1))
+        return max(rob1(nums[1:]), rob1(nums[:-1]))
