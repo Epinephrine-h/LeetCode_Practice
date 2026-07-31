fb25688b36803853325f1fe3d4aefdc8b19720bc
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-31 16:12:07 +0700

    Time: 94 ms (83.28%), Space: 20.2 MB (9.03%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -2,10 +2,4 @@ from collections import Counter
 class Solution:
     def minimumPushes(self, word: str) -> int:
         cnt = Counter(word)
-        ans = 0
-        for i, fre in enumerate(sorted(cnt.values(), reverse = True)):
-            if i < 8:   ans += fre
-            elif i < 16:    ans += fre * 2
-            elif i < 24:    ans += fre * 3
-            else:   ans += fre * 4
-        return ans
+        return sum(fre * (i // 8 + 1) for i, fre in enumerate(sorted(cnt.values(), reverse = True)))
