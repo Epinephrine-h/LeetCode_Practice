89b329ccb2b77860e2b650c58e9c301c7c334b9b
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-04 09:45:55 +0700

    Time: 7 ms (24.45%), Space: 19.4 MB (18.85%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,5 +1,7 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         nums.sort()
-        completetion = set(x for x in range(nums[0], nums[-1]))
-        return sorted(completetion - set(nums))
+        ans = []
+        for num in range(nums[0], nums[-1]):
+            if num not in nums:     ans.append(num)
+        return ans
