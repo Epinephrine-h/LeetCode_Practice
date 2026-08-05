ca54dbea8030ac53e9e96e8751a24f52d307f60c
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-05 13:00:06 +0700

    Time: 0 ms (100%), Space: 19.1 MB (93.19%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,12 +1,4 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        p2s = {}
-        s2p = {}
         wordList = s.split()
-        if len(pattern) != len(wordList):   return False
-        for ch, word in zip(pattern, wordList):
-            if (ch in p2s and p2s[ch] != word) or (word in s2p and s2p[word] != ch):
-                return False
-            p2s[ch] = word
-            s2p[word] = ch
-        return True
+        return len(pattern) == len(wordList) and len(set(pattern)) == len(set(wordList)) == len(set(zip(pattern, wordList)))
