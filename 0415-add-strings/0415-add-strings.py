363fad11dc7235b1285069840c79497158ad80b6
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-06 17:19:09 +0700

    Time: 0 ms (100%), Space: 19.5 MB (31.16%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -3,8 +3,8 @@ class Solution:
         i, j, carry = len(num1) - 1, len(num2) - 1, 0
         ans = []
         while i >= 0 or j >= 0 or carry > 0:
-            n1 = 0 if i < 0 else int(num1[i])
-            n2 = 0 if j < 0 else int(num2[j])
+            n1 = 0 if i < 0 else ord(num1[i]) - ord('0')
+            n2 = 0 if j < 0 else ord(num2[j]) - ord('0')
             sum = n1 + n2 + carry
             ans.append(str(sum % 10))
             carry = sum // 10
