5e600a3b8c7cc70d1be22234188a4c95e88e421e
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-08-08 08:33:17 +0700

    Time: 7 ms (78.34%), Space: 19.5 MB (91.08%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,13 +1,12 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        i, carry = len(num) - 1, 0
-        ans = []
-        while i >= 0 or k >0 or carry > 0:
-            num1 = 0 if i < 0 else num[i]
-            num2 = 0 if not k else k % 10
+        i = len(num) - 1
+        while i >= 0 and k > 0:
+            k += num[i]
+            num[i] = k % 10
             k //= 10
-            sum = num1 + num2 + carry
-            ans.append(sum%10)
-            carry = sum // 10
             i -= 1
-        return ans[::-1]
+        while k > 0:
+            num.insert(0, k % 10)
+            k //= 10
+        return num
